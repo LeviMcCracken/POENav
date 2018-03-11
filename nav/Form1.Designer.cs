9b06593c4d58ecc9d0b3f6691ff876fa674feb5a
Levi McCracken <levi.mccracken@gmail.com>
2018-03-10 19:55:38 -0700

    Map loading works.

--- nav/Form1.Designer.cs
+++ nav/Form1.Designer.cs
@@ -29,49 +29,138 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.human = new System.Windows.Forms.RichTextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanelRoot = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.human = new System.Windows.Forms.RichTextBox();
             this.absolute = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelRoot.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanelRoot
+            // 
+            this.tableLayoutPanelRoot.AutoSize = true;
+            this.tableLayoutPanelRoot.ColumnCount = 2;
+            this.tableLayoutPanelRoot.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelRoot.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelRoot.Controls.Add(this.tableLayoutPanel1, 1, 0);
+            this.tableLayoutPanelRoot.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanelRoot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelRoot.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelRoot.Name = "tableLayoutPanelRoot";
+            this.tableLayoutPanelRoot.RowCount = 1;
+            this.tableLayoutPanelRoot.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelRoot.Size = new System.Drawing.Size(1433, 630);
+            this.tableLayoutPanelRoot.TabIndex = 5;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.human, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.absolute, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.richTextBox2, 0, 3);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(280, 624);
+            this.tableLayoutPanel3.TabIndex = 1;
             // 
             // human
             // 
-            this.human.Location = new System.Drawing.Point(1, -2);
+            this.human.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.human.Location = new System.Drawing.Point(3, 3);
             this.human.Name = "human";
             this.human.ReadOnly = true;
-            this.human.Size = new System.Drawing.Size(345, 96);
-            this.human.TabIndex = 0;
+            this.human.Size = new System.Drawing.Size(274, 87);
+            this.human.TabIndex = 1;
             this.human.Text = "";
             // 
             // absolute
             // 
-            this.absolute.Location = new System.Drawing.Point(1, 100);
+            this.absolute.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.absolute.Location = new System.Drawing.Point(3, 96);
             this.absolute.Name = "absolute";
             this.absolute.ReadOnly = true;
-            this.absolute.Size = new System.Drawing.Size(345, 96);
-            this.absolute.TabIndex = 1;
+            this.absolute.Size = new System.Drawing.Size(274, 87);
+            this.absolute.TabIndex = 2;
             this.absolute.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 189);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(274, 87);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 282);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(274, 87);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(289, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1141, 624);
+            this.tableLayoutPanel1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1433, 630);
-            this.Controls.Add(this.absolute);
-            this.Controls.Add(this.human);
+            this.Controls.Add(this.tableLayoutPanelRoot);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "POENav";
+            this.tableLayoutPanelRoot.ResumeLayout(false);
+            this.tableLayoutPanelRoot.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.RichTextBox human;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelRoot;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.RichTextBox human;
         private System.Windows.Forms.RichTextBox absolute;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- nav/Form1.cs
+++ nav/Form1.cs
@@ -23,17 +23,20 @@ namespace nav
 
         long lastReadLength;
         FileStream fs;
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
 
         public Form1()
         {
-
+            DialogResult folder = fbd.ShowDialog();
             OpenFileDialog file = new OpenFileDialog();
+
+
             if (file.ShowDialog() == DialogResult.OK)
             {
                 path = file.FileName;
-                lastReadLength = 0;
 
                 fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                lastReadLength = fs.Length;
 
                 entryHumanStringLen = entryHumanString.Length;
                 entryAbsoluteStringLen = entryAbsoluteString.Length;
@@ -48,8 +51,7 @@ namespace nav
                 // Enable timer. 
                 timer1.Enabled = true;
             }
-
-
+            
         }
 
         private void timer1_tick(object sender, System.EventArgs e)
@@ -71,22 +73,112 @@ namespace nav
         private void parseLogSnippet(string s)
         {
             string[] lines = s.Split('\n');
+            string areaName = "";
+            string humanName = "";
             foreach (string line in lines)
             {
                 int index = line.IndexOf(entryHumanString);
                 if (index != -1)
                 {
-                    human.Text = line.Substring(index + entryHumanStringLen).Replace(" ", "").Replace("'","").Replace(".","");
+                    humanName = line.Substring(index + entryHumanStringLen).Replace(" ", "").Replace("'", "").Replace(".", "");
+                    human.Text = humanName;
                 }
                 int indexAbs = line.IndexOf(entryAbsoluteString);
                 if (indexAbs != -1)
                 {
-                    absolute.Text = line.Substring(indexAbs + entryAbsoluteStringLen);
+                    areaName = line.Substring(indexAbs + entryAbsoluteStringLen);
+                    absolute.Text = areaName;
+                    displayMap(areaName, humanName);
                 }
             }
+
+            
 
             //textBox.Text = area;
         }
 
+        private void displayMap(string areaName, string humanName)
+        {
+            string mapFolder = makeStringLegal(fbd.SelectedPath.ToString() + @"\" + areaName);
+
+            if (Directory.Exists(mapFolder))
+            {
+                try
+                {
+                    
+                    richTextBox1.Text = "";
+                    string[] files = Directory.GetFiles(mapFolder);
+
+                    tableLayoutPanel1.Controls.Clear();
+
+                    tableLayoutPanel1.ColumnCount = 1;
+                    tableLayoutPanel1.RowCount = 1;
+
+                    if (files.Length > 0)
+                    {
+
+                        int columns = (int)Math.Ceiling(Math.Sqrt(files.Length));
+                        int rows = columns;
+                        if ((rows - 1) * columns >= files.Length) { rows--; }
+                        if (rows == 0) { rows = 1; }
+
+                        richTextBox2.Text = columns + ":" + rows;
+
+                        tableLayoutPanel1.ColumnCount = columns;
+                        tableLayoutPanel1.RowCount = rows;
+
+
+                        int i = 0;
+                        foreach (string file in files)
+                        {
+                            richTextBox1.Text += file.ToString() + "\n";
+                            PictureBox pb = new PictureBox();
+                            pb.ImageLocation = file;
+                            pb.SizeMode = PictureBoxSizeMode.Zoom;
+                            pb.Dock = DockStyle.Fill;
+
+                            tableLayoutPanel1.Controls.Add(pb, i % columns, i / columns);
+                            i++;
+                        }
+                        
+                    }
+                }
+                catch (Exception e)
+                {
+                    richTextBox1.Text = e.ToString();
+                    richTextBox1.Text += "\n" + mapFolder;
+                }
+            }
+            else
+            {
+                using (StreamWriter outputFile = new StreamWriter(fbd.SelectedPath + "\\nameMap.txt", true))
+                {
+                    outputFile.WriteLine(areaName + " " + humanName);
+                }
+                Directory.CreateDirectory(mapFolder);
+            }
+        }
+
+        private string makeStringLegal(string s)
+        {
+            string invalid = new string(Path.GetInvalidPathChars());
+            
+            foreach (char c in invalid)
+            {
+                s = s.Replace(c.ToString(), "");
+            }
+
+            return s;
+        }
+
+        private void human_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
